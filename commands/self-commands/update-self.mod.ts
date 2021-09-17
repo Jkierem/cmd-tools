8b24ca4478784ce0f707ed857a31b1e7e4275a2e
Jkierem <juanmiguelgomez8@gmail.com>
2021-09-17 17:31:15 -0500

    Added fix for windows

--- commands/self-commands/update-self.mod.ts
+++ commands/self-commands/update-self.mod.ts
@@ -1,10 +1,11 @@
 import IOProcess from "../../core/io-process.mod.ts"
+import IOPromise from "../../core/io-promise.mod.ts"
 import { Command } from "../../core/command.mod.ts"
 import { resolveFolder } from "../../core/resolve.mod.ts"
 import { pullBranch } from "../../core/git-helpers.mod.ts"
 
 const pwd = IOProcess.of(["pwd"]).map(x => x.trim())
-const cd = (to: string) => IOProcess.of(["cd",to])
+const cd = (to: string) => IOPromise.from(() => Deno.chdir(to))
 
 const UpdateSelf = Command
     .ask<{ fileUrl: string }>()
